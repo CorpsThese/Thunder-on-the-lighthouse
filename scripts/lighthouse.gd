992d35ca024ad65162d6ef94e9debbf0cffb7829
Cortez <cortez.reseaux@gmail.com>
2024-09-11 19:21:35 +0200

    Tileset overall

--- scripts/lighthouse.gd
+++ scripts/lighthouse.gd
@@ -5,7 +5,7 @@ extends Node
 
 @onready var ghost_spawn_position: PathFollow2D = %GhostSpawnPosition
 var ghost_counter := 0
-const max_ghost := 0
+const max_ghost := 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
