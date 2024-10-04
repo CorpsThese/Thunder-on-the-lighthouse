8f92fd43d6b8b9829122a701a1d1803be21437ae
Cortez <cortez.reseaux@gmail.com>
2024-10-04 14:34:51 +0200

    Fancy endscreen

--- scripts/lighthouse.gd
+++ scripts/lighthouse.gd
@@ -49,7 +49,7 @@ func _on_shadow_spawn_timer_timeout() -> void:
 func delete_shadow(shadow_node: CharacterBody2D) -> void:
 	shadow_node.queue_free()
 	$ShadowDefeated.play()
-	%Child.damage(-7)
+	%Child.damage(-6)
 	shadow_counter -= 1
 	$ShadowSpawnTimer.start()
 	if is_wave_on:
